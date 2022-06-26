8b93a71eb5d76a7f82b2ae367d1737048d2fb695
Kowacka Malgorzata <kowackag@gmail.com>
2022-06-26 12:46:09 +0200

    Create customHook useModal

--- src/components/organisms/Modal/Modal.js
+++ src/components/organisms/Modal/Modal.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
+import { ModalWrapper } from './Modal.styled';
+import { Button } from 'components/atoms/Button/Button';
 const modalContainer = document.getElementById('modal-container');
 
-const Modal = () => {
+const Modal = ({ children, handleClose }) => {
   const modalNode = document.createElement('div');
 
   useEffect(() => {
@@ -13,7 +15,13 @@ const Modal = () => {
     };
   });
 
-  return ReactDOM.createPortal(<div>fff</div>, modalNode);
+  return ReactDOM.createPortal(
+    <ModalWrapper>
+      {children}
+      <Button onClick={handleClose}>Close</Button>
+    </ModalWrapper>,
+    modalNode
+  );
 };
 Modal.propTypes = {};
 export default Modal;
